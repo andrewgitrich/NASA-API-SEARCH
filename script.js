342364d746becdc105d49a6804075729f09899d1
andrew <andrew_rich@hotmail.com>
2016-10-20 11:14:06 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -6,30 +6,23 @@ $("iframe").hide();
 //grab value from form
 /*$("#search-term").submit(function(event){
 event.preventDefault();*/
-(function go(){
+
+//gets todays photo of the day immediatly
+(function(){
 var inputDate = $("#query").val();
 	getRequest(inputDate);
 	$("#query").val("");
 })();
 
-//random button
+//random button creates random num for month,day,year
 $("#random").on("click", function(){
 	var year = Math.floor(Math.random() * (2016 - 2001) + 2001);
  	var month = Math.floor(Math.random() * (13 - 1) + 1);
 	var day = Math.floor(Math.random() * (29 - 1) + 1);
 	var randomDate = year.toString() + "-" + month.toString() + "-" + day.toString();
-/*console.log(year);
-console.log(month);
-console.log(day);
-console.log(randomDate);*/
+
 	getRequest(randomDate);
 });
- 
-
-
-
-
-
 
 //run get request with value from form
 function getRequest(date){
@@ -39,7 +32,6 @@ function getRequest(date){
 			api_key: "VHh1uMGyZiUyQqrCSirGZxoOZju4EHBqRWyODt31" 
 		};
 		
-
 	$.ajax({
 		url: "https://api.nasa.gov/planetary/apod",
 		data: params,
@@ -57,22 +49,21 @@ function getRequest(date){
 //display results
 function showGetResults(result){
 
-
+//if a vid returns run
 	if(result.media_type === "video") {
 	   	$("#img-api").hide();
 	    $("iframe").show();
 	    $("#vid-api").attr("src", result.url);
-  	}
+  		}
+ //if an img returns run
 	else {
 		$("#img-api").show();
 	    $("#vid-api").css("display", "none"); 
-	 	//$("#img-api").attr("src", result.hdurl);
 	 	$("#container").css({"background-image": 'url(' + result.hdurl + ')','background-repeat': 'no-repeat','background-size': '100%' });
   			console.log(result.hdurl);
   		}
 
-  		
-
+//add a title and description
   		$("#demo").text(result.explanation);
 		$("#title").text(result.title);
 	}
