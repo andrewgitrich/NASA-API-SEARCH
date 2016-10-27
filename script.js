65a66341b2b848db99fe0b85436fb3ae41948f48
andrew <andrew_rich@hotmail.com>
2016-10-27 08:16:35 -0400

    added date search, style

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ $("iframe").hide();
 
 //grab value from form date search
 $("#search-term").submit(function(event){
-event.preventDefault();
+	event.preventDefault();
 	getRequest($("#query").val());
-	});
+});
 
 //gets todays photo of the day immediatly
 (function(){
-var inputDate = $("#query").val();
+	var inputDate = $("#query").val();
 	getRequest(inputDate);
 	$("#query").val("");
 })();
@@ -29,49 +29,44 @@ $("#random").on("click", function(){
 //run get request with value from form
 function getRequest(date){
 		var params ={
-			date: date,
-
-			//hd: "",	
+			date: date,	
 			api_key: "VHh1uMGyZiUyQqrCSirGZxoOZju4EHBqRWyODt31" 
-		};
+			};
 		
-	$.ajax({
-		url: "https://api.nasa.gov/planetary/apod",
-		data: params,
-		dataType: "json",
-		type: "GET",
-		})
+		$.ajax({
+			url: "https://api.nasa.gov/planetary/apod",
+			data: params,
+			dataType: "json",
+			type: "GET",
+			})
 
-	.done(function(result){
-		var getResults = showGetResults(result);
-		console.log(result);
-		
-	});
-}
+		.done(function(result){
+			var getResults = showGetResults(result);
+			console.log(result);	
+			});
+	}
+
 	
 //display results
 function showGetResults(result){
 
-//if a vid returns run
+	//if a vid returns run
 	if(result.media_type === "video") {
 	   	$("#container").hide();
 	   	$("iframe").show();
-	    //$("iframe").css({"background-image": 'url(' + result.url + ')','background-repeat': 'no-repeat','background-size': '100%' });
 	    $("#vid-api").attr("src", result.url);
   		}
- //if an img returns run
+
+ 	//if an img returns run
 	else {
 		$("#container").show();
 	    $("#vid-api").css("display", "none"); 
 	 	$("#container").css({"background-image": 'url(' + result.hdurl + ')','background-repeat': 'no-repeat','background-size': '100%' });
-  			console.log(result.hdurl);
   		}
 
-//add a title and description
+		//add a title and description
   		$("#demo").text(result.explanation);
 		$("#title").text(result.title);
 	}
-
-
 });
  
